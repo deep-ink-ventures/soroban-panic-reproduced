c0d0701a0b9be2d5564314bc38a9b55779785c9e
deep-ink-ventures <chp@deep-ink.ventures>
2023-07-17 10:11:10 +0200

    init

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, contracterror, panic_with_error, Env};
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    Whatever = 42,
+}
+
+#[contract]
+pub struct Contract;
+
+#[contractimpl]
+impl Contract {
+    pub fn raise_error(env: Env) {
+        panic_with_error!(&env, Error::Whatever);
+    }
+}
+
+#[test]
+#[should_panic(expected = "Status(ContractError(42))")]
+fn test_panic() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, Contract);
+    let client = ContractClient::new(&env, &contract_id);
+
+    client.raise_error();
+}
